986ca7a800d7753328a90e62e63e52fa6b1af77a
성우 <65459616+ABizCho@users.noreply.github.com>
2021-09-17 09:50:49 +0000

    pointer & swap Practice

---
+++ p10_pointer2swap.cpp
@@ -0,0 +1,28 @@
+//사용자정의함수와 pointer를 이용해 두 변수의 값을 치환하는 프로그램 
+
+
+#include <iostream>
+using namespace std;
+
+void swap(int* n1,int* n2)
+{
+  int temp ;
+  
+  temp = *n1;
+  *n1 = *n2;
+  *n2 = temp;
+  
+}
+
+int main()
+{
+  
+  int v1 = 10;
+  int v2 = 20;
+  
+  cout << "original v1: "<< v1 <<" | v2 : "<<v2<<endl;
+  swap(&v1,&v2);
+  cout << "swaped v1: "<< v1 <<" | v2 : "<<v2<<endl;
+  
+  return 0;
+}
